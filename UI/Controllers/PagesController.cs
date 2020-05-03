67a34fb0be867e63026c3d2f60994282feaceb1c
cguillencr <cris.guillen0@gmail.com>
2020-05-03 14:13:43 -0600

    Lets add log4net to the UI project in order to figured out how this application bahaves when the API doesnt respond.

--- UI/Controllers/PagesController.cs
+++ UI/Controllers/PagesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LogsProcesor;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using RestSharp;
@@ -19,13 +20,28 @@ namespace UI.Controllers
 		[HttpGet("/page1")]
 		public IActionResult Page1()
 		{
-			string apiServer = configuration.GetValue<string>("ApiServerUrl");
+			try
+			{
+				string apiServer = configuration.GetValue<string>("ApiServerUrl");
 
-			var client = new RestClient($"http://{apiServer}/api/values");
-			client.Timeout = 100;
-			var request = new RestRequest(Method.GET);
-			IRestResponse response = client.Execute(request);
-			return Ok(response.Content);
+				var client = new RestClient($"http://{apiServer}/api/values");
+				client.Timeout = 100;
+				var request = new RestRequest(Method.GET);
+				IRestResponse response = client.Execute(request);
+
+				if ( !response.IsSuccessful )
+				{
+					Logger.Debug($"API error. Status: {response.StatusCode} |  Content: {response.Content} |  ErrorMessage: {response.ErrorMessage} |  ErrorMessage: {response.ErrorException}");
+				}
+
+				return Ok(response.Content);
+			}
+			catch (Exception e)
+			{
+				Logger.Error(e);
+				throw e;
+			}
+			
 		}
 	}
 }
